a7b72e3c68d2262d3974c5b514e1b410f6a5da2e
Pawel Osobinski <p.osobinski@gmail.com>
2021-10-26 23:22:10 +0200

    Update virtualenv.sh

--- virtualenv.sh
+++ virtualenv.sh
@@ -2,20 +2,12 @@
 
 ENV_NAME=virtualenv
 
-BASE_PATH=`basename $0`
-
-if [ "echo $0 | cut -c1" = "/" ]; then
- TOOLS_PATH=`dirname $0`
-else
- TOOLS_PATH=`pwd`/`echo $0 | sed -e s/$BASE_PATH//`
-fi
-
 virtualenv $ENV_NAME
 
 if [ $? -eq 0 ]; then
     source $PWD/$ENV_NAME/bin/activate
     python3 -m pip install --upgrade pip
-    pip3 install -r $TOOLS_PATH/requirements.txt
+    pip3 install -r $PWD/requirements.txt
     grep -qxF "source $PWD/$ENV_NAME/bin/activate" $HOME/.bashrc || echo "source $PWD/$ENV_NAME/bin/activate" >> $HOME/.bashrc
     
 else
