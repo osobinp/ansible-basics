d174232f7de28997ca1817d449f372e827bd17f7
osobinp <osobinp@syjon.net>
2022-11-18 18:14:10 +0100

    Added new labs

--- virtualenv.sh
+++ virtualenv.sh
@@ -9,7 +9,8 @@ if [ $? -eq 0 ]; then
     python3 -m pip install --upgrade pip
     pip3 install -r $PWD/requirements.txt
     grep -qxF "source $PWD/$ENV_NAME/bin/activate" $HOME/.bashrc || echo "source $PWD/$ENV_NAME/bin/activate" >> $HOME/.bashrc
-    
+    source $PWD/$ENV_NAME/bin/activate
+    ansible-galaxy collection install ansible.posix community.general
 else
     echo "Install python3-pip && pip3 install virtualenv"
 fi
